19a308681a9cf568851ef4ddc2d18ce11180a545
Siddharth Shankar Das <siddharthshankardas15@gmail.com>
2025-11-15 10:17:54 +0530

    Implement keyboard shortcuts and reset confirmation.
    Added keyboard shortcuts for game actions.
    
    Added reset confirmation functionality with alert notification.
    
    Also added some description/notes to understand clearly.

--- Logic/rock-paper-scissor.js
+++ Logic/rock-paper-scissor.js
@@ -1,6 +1,20 @@
 // Get the score from localStorage or initialize it
 const score = JSON.parse(localStorage.getItem('score')) || { win: 0, lose: 0, tie: 0 };
 
+// Added Shortcuts for most functionality.
+document.body
+  .addEventListener('keydown', (event) => {
+    const key = event.key.toLowerCase();
+    if (key === 'r') playGame('Rock');
+    else if (key === 'p') playGame('Paper');
+    else if (key === 's') playGame('Scissors');
+    else if (key === 'x') showResetConfirmation();
+    else if (key === 'a') autoPlay();
+    else if (key === 'y') { resetScoreBoard(); hideAlertConfirmation(); }
+    else if (key === 'n') hideAlertConfirmation();
+  });
+
+// Main functionality to play the game.
 function playGame(playerChoice) {
   // Validate player choice
   const choices = ['Rock', 'Paper', 'Scissors'];
@@ -27,6 +41,7 @@ function playGame(playerChoice) {
   // Display the result on the page
   document.querySelector('.js-result').innerText = result;
 
+  // Update the result in HTML
   document.querySelector('.js-moves')
     .innerHTML = `You <img src="./Images/${playerChoice}-emoji.png" class="moves-images"> <img src="./Images/${computerChoice}-emoji.png"  class="moves-images"> Computer`;
 
@@ -43,11 +58,36 @@ function resetScoreBoard() {
   localStorage.removeItem('score'); // Clear the score from localStorage
 
   updateScoreElement();
+}
 
-  // OR
+// Added eventListener for score reset confirmation.
+document.querySelector('.reset-score-button')
+  .addEventListener('click', showResetConfirmation);
 
-  // Update the score in localStorage
-  // localStorage.setItem('score', JSON.stringify(score));
+// Added reset confirmation message.
+function showResetConfirmation() {
+  document.querySelector('.js-reset-confirmation')
+		.innerHTML = `
+			Are you sure you want to reset the score?
+			<button class="js-del-conf-yes">Yes</button>
+			<button class="js-del-conf-no">No</button>
+		`;
+
+  document.querySelector('.js-del-conf-yes')
+    .addEventListener('click', () => {
+      resetScoreBoard();
+      hideAlertConfirmation();
+    });
+
+  document.querySelector('.js-del-conf-no')
+    .addEventListener('click', () => {
+      hideAlertConfirmation();
+    });
+}
+
+// Hide alert notification for reset.
+function hideAlertConfirmation() {
+  document.querySelector('.js-reset-confirmation').innerHTML =  ``;
 }
 
 // Function to update the score element on the page
@@ -58,6 +98,7 @@ function updateScoreElement() {
 // Display the score on the page
 updateScoreElement();
 
+// Add functionality to change the button element text.
 function changeButtonEleText() {
   const buttonElement = document.querySelector(".js-auto-play-button");
 
@@ -68,9 +109,11 @@ function changeButtonEleText() {
   }
 }
 
+// Global variables for autoplay functionality.
 let isAutoPlaying = false;
 let intervalId;
 
+// Adding autoplay functionality to play & stop automatically.
 function autoPlay() {
   const buttonElement = document.querySelector(".js-auto-play-button");
 
@@ -89,3 +132,5 @@ function autoPlay() {
     isAutoPlaying = false;
   }
 }
+
+
